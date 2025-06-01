42da95338afbbf46d0279716a55aee9f21ab3e89
Kartik garg <krigrg527@gmail.com>
2025-06-01 07:50:48 +0530

    agent created

---
+++ convex/agent.ts
@@ -0,0 +1,51 @@
+import { openai } from "@ai-sdk/openai";
+import { Agent } from "@convex-dev/agent";
+import { v } from "convex/values";
+import { components } from "./_generated/api";
+import { action } from "./_generated/server";
+import { z } from "zod";
+
+const createThreadSchema = z.object({
+  prompt: z.string(),
+  code: z.string(),
+});
+
+const supportAgent = new Agent(components.agent, {
+  chat: openai.chat("gpt-4o-mini"),
+  textEmbedding: openai.embedding("text-embedding-3-small"),
+  instructions:
+    "You are a code review assistant that is a Senior Software Engineer. Your goal is to help developers review their code by providing constructive feedback, identifying potential issues, and suggesting improvements. Focus on code quality, best practices, and potential bugs but avoid being too basic.",
+  tools: {},
+});
+
+export const supportAgentStep = action({
+  args: {
+    threadId: v.string(),
+    message: v.string(),
+  },
+  handler: async (ctx, { message }) => {
+    const { thread } = await supportAgent.createThread(ctx);
+    return thread.generateText({ prompt: message });
+  },
+});
+
+export const createCodeReviewThread = action({
+  args: {
+    prompt: v.string(),
+    code: v.string(),
+  },
+  handler: async (ctx, args) => {
+    //validate with zod
+    const { prompt, code } = createThreadSchema.parse(args);
+
+    const { threadId, thread } = await supportAgent.createThread(ctx);
+
+    const result = await thread.generateText({
+      prompt: `${prompt}\n\nHere's the code to review:\n\`\`\`\n${code}\n\`\`\``,
+    });
+
+    console.log(result.text);
+
+    return { threadId, text: result.text };
+  },
+});
