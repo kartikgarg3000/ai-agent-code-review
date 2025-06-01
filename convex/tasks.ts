e0d80bced7e11225fe79f33d6d208101c84ad05e
Kartik garg <krigrg527@gmail.com>
2025-06-01 06:33:15 +0530

    Add more components

--- convex/tasks.ts
+++ convex/tasks.ts
@@ -3,6 +3,7 @@ import { query } from "./_generated/server";
 export const get = query({
   args: {},
   handler: async (ctx) => {
+    console.log("Getting tasks");
     return await ctx.db.query("tasks").collect();
   },
 });
